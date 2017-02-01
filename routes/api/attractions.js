1bd233d47f19c098c46a204e2b97413ddb684f5a
Jason Lam <jaylam111@gmail.com>
2017-02-01 18:27:22 -0500

    ajax on the console

--- routes/api/attractions.js
+++ routes/api/attractions.js
@@ -6,10 +6,49 @@ var Activity = require('../../models/activity');
 
 
 router.get('/hotels', (req, res, next) => {
-// res.send('hello')
+
   Hotel.findAll()
   .then((hotels) => {
     res.json(hotels)
+  })
+})
+
+// $.ajax({
+//   method: 'VERB',
+//   url: '/whatever/route',
+//   data: someDataToSend, // e.g. for POST requests
+// })
+// .then(function (responseData) {
+//   // some code to run when the response comes back
+// })
+// .catch(function (errorObj) {
+//   // some code to run if the request errors out
+// });
+
+// $.ajax({
+//   method: 'GET',
+//   url: '/hotels'
+// })
+// .then(() => {
+//   return Hotel.findAll()
+//   })
+//   .then(hotels => {
+//     res.json(hotels)
+// })
+// .catch(err)
+
+router.get('/restaurants', (req, res, next) => {
+  Restaurant.findAll()
+  .then((restaurants) => {
+    res.json(restaurants)
+  })
+})
+
+router.get('/activities', (req, res, next) => {
+
+  Activity.findAll()
+  .then((activities) => {
+    res.json(activities)
   })
 })
 
